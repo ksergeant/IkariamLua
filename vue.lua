5c428f6a44eefff1f2d23207508e2086459511dc
ksergeant <kevin.sergeant.pro@gmail.com>
2019-07-08 22:13:24 +0200

    Amélioration de la vue ressources

--- vue.lua
+++ vue.lua
@@ -8,7 +8,10 @@ function vue:Create(pNom)
  
   function vueTempo:Draw()
 
+    local r,g,b = love.graphics.getColor() 
+    love.graphics.setColor(0,0,0)
     love.graphics.print("Voici la vue "..self.nom,40,100)
+    love.graphics.setColor(r,g,b)
     --print("affichage"..self.nom)
   
   end
@@ -23,14 +26,20 @@ end
 
 function vue:Ressources(pListeData)
 
-  love.graphics.print("Villes", 85, 79)
+  local r,g,b = love.graphics.getColor() 
+  love.graphics.setColor(0,0,0)
+
+  love.graphics.print("VILLES", 85, 79)
+  love.graphics.line(0, 100, 1440, 100)
+  love.graphics.setColor(r,g,b)
+
   local imageBois = FunctionPicture:Create("Bois", 300, 70,  "Images/Bois.png", 1,1)
   local imageVin = FunctionPicture:Create("Vin", 400, 70,  "Images/Vin.png",1,1)
   local imageMarbre = FunctionPicture:Create("Marbre", 500, 70,  "Images/Marbre.png",1,1)
   local imageCristal = FunctionPicture:Create("Cristal", 600, 70,  "Images/Cristal.png", 1,1)
   local imageSouffre = FunctionPicture:Create("Souffre", 700, 70,  "Images/Souffre.png",1,1)
-  local imageOr = FunctionPicture:Create("Or", 800, 70,  "Images/Or.png",1,1)
-  local imageUpdate = FunctionPicture:Create("Update", 900, 70,  "Images/Update.png",1,1)
+  local imageUpdate = FunctionPicture:Create("Update", 800, 70,  "Images/Update.png",1,1)
+  local imageOr = FunctionPicture:Create("Or", 900, 70,  "Images/Or.png",1,1)
 
   local listeData = pListeData
   
@@ -47,13 +56,17 @@ function vue:Ressources(pListeData)
 
    for i = 1, nombreVilles do 
 
+    local r,g,b = love.graphics.getColor() 
+    love.graphics.setColor(1,0,0)
     love.graphics.print(listeData.Villes[i]._nom, 85, 79 + decalage)
+    love.graphics.setColor(r,g,b)
+
     love.graphics.print(listeData.Villes[i]._nbBois, 300, 79 + decalage)
     love.graphics.print(listeData.Villes[i]._nbVin, 400, 79 + decalage)
     love.graphics.print(listeData.Villes[i]._nbMarbre, 500, 79 + decalage)
     love.graphics.print(listeData.Villes[i]._nbCristal, 600, 79 + decalage)
     love.graphics.print(listeData.Villes[i]._nbSouffre, 700, 79 + decalage)
-    love.graphics.print(listeData.Villes[i]._update, 900, 79 + decalage)
+    love.graphics.print(listeData.Villes[i]._update, 800, 79 + decalage)
     
     decalage = decalage+ 50
 
